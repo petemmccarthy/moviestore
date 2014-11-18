3e989ad72ecbaa1ac4353d250f468961ca7a8cf4
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-18 16:33:00 +0000

    foundation-rails gem added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :movies, only: [:show, :index]
+  resources :movies #, only: [:show, :index]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
