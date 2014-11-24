462c327ec3de63a6cc166792d24bf417d668b798
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-24 10:40:27 +0000

    shopping cart controller created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'carts/show'
+
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :movies #, only: [:show, :index]
 
