72d43b3b1ef15751eb3019ad69254b8f1fd00e1d
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-11-24 15:23:43 +0000

    cart_action instance object created

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,4 +8,12 @@ class Movie < ActiveRecord::Base
     "http://www.imdb.com/title/#{imdb_id}/"
   end
 
+  def cart_action(current_user_id)
+    if $redis.smember "cart#{current_user_id}", id
+      "Remove from"
+    else
+      "Add to"
+    end
+  end
+
 end
